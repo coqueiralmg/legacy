8a3e75b51c5c40a469ba5f1e56a7fe9d42fb8808
Fábio Valentim <maitabom@gmail.com>
2017-03-03 10:08:38 -0300

    Configuração no controle de especial

--- app/controllers/especial.php
+++ app/controllers/especial.php
@@ -1,8 +1,14 @@
 <?php
 
 $app->get("/especial/pesquisa_habitacional", function() use($twig){
+    
+    // objeto SecretariaDAO
+	$secretariaDAO = new \app\models\SecretariaDAO();
+	$secretarias = $secretariaDAO->listar(14, 0);
+    
     $dados = array(
-		"title" => "Pesquisa Habitacional | ",
+		"title" => "Pesquisa de Demanda Habitacional | ",
+        "secretarias" => $secretarias
 	);
 
 	$twig->loadTemplate("pesquisa_habitacional.html")->display($dados);
