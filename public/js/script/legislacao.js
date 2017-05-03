4b078bf6b6e2ba61830a5686f8a3d5c0f6af50da
Fábio Valentim <maitabom@gmail.com>
2017-05-03 11:08:59 -0300

    Ajustes no registro de eventos de busca

--- public/js/script/legislacao.js
+++ public/js/script/legislacao.js
@@ -5,6 +5,7 @@ $(function(){
 		e.preventDefault();
 		var busca = $(this).siblings("#pesquisa").val();
 		if(busca !== ''){
+			ga('send', 'event', 'Publicações', 'Busca', busca);
 			window.location.href = "/publicacoes/page/1/limit/10/search/" + busca;
 		}else{
 			alert('Digite sua busca!');
