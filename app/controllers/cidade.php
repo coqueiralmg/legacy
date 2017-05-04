a8f5a9263e3e8dc9d79588e45dab44193152b7b9
Fábio Valentim <maitabom@gmail.com>
2017-05-04 13:46:31 -0300

    Criação da tela de localização da cidade

--- app/controllers/cidade.php
+++ app/controllers/cidade.php
@@ -13,3 +13,17 @@ $app->get("/cidade/historico", function() use($twig){
 
 	$twig->loadTemplate("historico.html")->display($dados);
 });
+
+$app->get("/cidade/localizacao", function() use($twig){
+    
+    // objeto SecretariaDAO
+	$secretariaDAO = new \app\models\SecretariaDAO();
+	$secretarias = $secretariaDAO->listar(14, 0);
+    
+    $dados = array(
+		"title" => "Localização da Cidade | ",
+        "secretarias" => $secretarias
+	);
+
+	$twig->loadTemplate("localizacao.html")->display($dados);
+});
