fa8c425a18b7442037f22cf4da2610d9eaa52c6b
Fábio Valentim <maitabom@gmail.com>
2017-05-31 16:00:08 -0300

    Ajustes no monitoramento do site

--- loggies.php
+++ loggies.php
@@ -1,7 +1,5 @@
 <?php
 
-//echo dirname(__FILE__) . '/../le_php-master/logentries.php';
-
 if($_SERVER["SERVER_NAME"] == "coqueiral.mg.gov.br" || $_SERVER["SERVER_NAME"] == "www.coqueiral.mg.gov.br") {
 
     require_once ROOT . 'vendor/le_php-master/logentries.php';
