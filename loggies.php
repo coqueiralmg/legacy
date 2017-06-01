482327e024198ca4a1114ef41b2bcaa0850932d2
Fábio Valentim <maitabom@gmail.com>
2017-06-01 16:00:53 -0300

    Ajustes para que seja monitoramento de login no painel administrativo fora da rede da prefeitura

--- loggies.php
+++ loggies.php
@@ -3,6 +3,8 @@
 if($_SERVER["SERVER_NAME"] == "coqueiral.mg.gov.br" || $_SERVER["SERVER_NAME"] == "www.coqueiral.mg.gov.br") {
 
     require_once ROOT . 'vendor/le_php-master/logentries.php';
+
+    $ip_allow = ["187.108.112.168"];
     
     $ip = $_SERVER["REMOTE_ADDR"];
     $agent = $_SERVER['HTTP_USER_AGENT'];
@@ -15,5 +17,36 @@ if($_SERVER["SERVER_NAME"] == "coqueiral.mg.gov.br" || $_SERVER["SERVER_NAME"] =
     ];
 
     $log->Info(json_encode($data));
-    
+
+    //Log do Admin
+    if($_SERVER['REQUEST_URI'] == "/admin" && !in_array($_SERVER["REMOTE_ADDR"], $ip_allow)) {
+
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $message = "Tentativa de acesso ao site fora da rede da prefeitura.";
+
+        $data = [
+            "ip" => $ip,
+            "agent" => $agent,
+            "message" => $message
+        ];
+
+        $log->Notice(json_encode($data));
+	}
+
+    //Login do Admin
+    if($_SERVER['REQUEST_URI'] == "/admin/logar" && !in_array($_SERVER["REMOTE_ADDR"], $ip_allow)) {
+        
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $user = $_POST["login"];
+        $message = "O usuário $user acessou o painel administrativo fora da rede da prefeitura.";
+
+        $data = [
+            "ip" => $ip,
+            "agent" => $agent,
+            "user" => $user,
+            "message" => $message
+        ];
+    }
 }
