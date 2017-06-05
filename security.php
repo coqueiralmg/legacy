08d7ac345b51d6be86bdca46a361d9094d738c23
Fábio Valentim <maitabom@gmail.com>
2017-06-05 13:11:12 -0300

    Redirecionamento para casos de acesso a diretórios inválidos e suspeitos

--- security.php
+++ security.php
@@ -11,3 +11,21 @@ if(in_array($_SERVER['HTTP_USER_AGENT'], $invalid_agent)){
     header("Location: http://127.0.0.1");
     die();
 }
+
+/*
+REDIRECIONAMENTO DE ACESSO POR DIRETÓRIOS INVÁLIDOS
+*/
+
+$path_patterns =[
+    "/log/interspire_6.1.4/",
+    "/filter.php"
+];
+
+$request = $_SERVER['REQUEST_URI'];
+
+foreach ($path_patterns as $pattern){
+    if(strstr($request, $pattern)) {
+        header("Location: http://127.0.0.1");
+        die();
+    }    
+}
