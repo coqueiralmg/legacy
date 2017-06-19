1e9e963cfd37b11c7e7bcc338395a99381a1bdcb
Fábio Valentim <maitabom@gmail.com>
2017-06-19 09:10:53 -0300

    Criação de script de limpeza de pasta cache

---
+++ clear.php
@@ -0,0 +1,33 @@
+<?php
+
+function unlinkRecursive($dir, $deleteRootToo) 
+{ 
+    if(!$dh = @opendir($dir)) 
+    { 
+        return; 
+    } 
+    while (false !== ($obj = readdir($dh))) 
+    { 
+        if($obj == '.' || $obj == '..') 
+        { 
+            continue; 
+        } 
+
+        if (!@unlink($dir . '/' . $obj)) 
+        { 
+            unlinkRecursive($dir.'/'.$obj, true); 
+        } 
+    } 
+    closedir($dh); 
+    if ($deleteRootToo) 
+    { 
+        @rmdir($dir); 
+    } 
+    return; 
+} 
+
+
+unlinkRecursive('cache', false);
+unlinkRecursive('log', false);
+
+echo 'Pastas limpas com sucesso!';
