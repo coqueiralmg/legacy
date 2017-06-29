78480ee79dd7d3ba660d0dd0bc9f518389fc85fd
Fábio Valentim <maitabom@gmail.com>
2017-06-29 15:57:20 -0300

    ALterações de configurações

--- config.php
+++ config.php
@@ -6,6 +6,14 @@ ini_set("display_errors", 1);
 ini_set("file_uploads", 1);
 ini_set("upload_tmp_dir", $_SERVER["DOCUMENT_ROOT"] . "/tmp");
 
+/*
+//Extensões
+
+ini_set("extension", "json.so");
+ini_set("extension", "pdo.so");
+ini_set("extension", "php_pdo_mysql.so");
+*/
+
 date_default_timezone_set ('America/Sao_Paulo');
 //define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/kit-lotofacil/");
 define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");
