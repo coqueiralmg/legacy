ea3a1b8df1be1a5f6a7e9328f4d7147a7f54c78b
Fábio Valentim <maitabom@gmail.com>
2017-07-03 12:45:27 -0300

    Retirada das configurações desnecessárias do servidor

--- config.php
+++ config.php
@@ -1,17 +1,8 @@
 <?php
-session_save_path($_SERVER["DOCUMENT_ROOT"] . "/tmp"); 
 session_start();
 
 ini_set("display_errors", 1);
 ini_set("file_uploads", 1);
-
-/*
-//Extensões
-
-ini_set("extension", "json.so");
-ini_set("extension", "pdo.so");
-ini_set("extension", "php_pdo_mysql.so");
-*/
 
 date_default_timezone_set ('America/Sao_Paulo');
 //define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/kit-lotofacil/");
